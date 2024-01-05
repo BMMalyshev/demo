33a2f7d3ef1ce5bf12faf3d649ce8ecd1005ea0e
BMM <bjorn19841028@gmail.com>
2024-01-05 10:30:12 +0300

    стер тестовое

--- src/Alishev/TestLambda/MyOwnTest.java
+++ src/Alishev/TestLambda/MyOwnTest.java
@@ -13,13 +13,8 @@ public class MyOwnTest {
         for (int i = 0; i < 100; i++) {
             arr.add(100 - i);
         }
-        List<Integer> sortedArr = new ArrayList<>();
-        sortedArr = arr;
-        System.out.println(sortedArr.toString());
 
-        sortedArr = arr.stream().sorted().toList();
         long b = currentTimeMillis();
         System.out.println(b - a);
-        System.out.println(sortedArr.toString());
     }
 }
